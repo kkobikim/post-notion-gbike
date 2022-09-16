69148495e248dc94085b9d03bd255ce4e084dda3
kkobikim <108338151+kkobikim@users.noreply.github.com>
2022-09-16 17:13:40 +0900

    2

--- site.config.ts
+++ site.config.ts
@@ -17,9 +17,7 @@ export default siteConfig({
   description: 'gbike',
 
   // social usernames (optional)
-  // twitter: 'transitive_bs',
-  // github: 'transitive-bullshit',
-  // linkedin: 'fisch2',
+
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
@@ -49,16 +47,28 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: '세소식',
+      pageId: '0adf0895b584482688e8d81e415d902b'
+    },
+    {
+      title: '인사이드',
+      pageId: '373a8997cfa041059b1aad9150e701ef'
+    },
+    {
+      title: '세소식',
+      pageId: 'dd95b6d843f344689266489efa4c337d'
+    },
+    {
+      title: '언론뉴스',
+      pageId: 'e7af798c90df42aebc1d6bd18d9d472f'
+    },
+    {
+      title: '창업',
+      pageId: '25772071fcdb44328dd1c2d9e3a1f18c'
+    }
+  ]
 })
